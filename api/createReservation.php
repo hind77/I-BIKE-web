53da465dca404cd0ab195036581398d116eb4c7c
Amine <elouattar@gmail.com>
2018-03-03 14:30:23 +0000

    Fix error

--- api/createReservation.php
+++ api/createReservation.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$response = array();
 
 	$query = $db->query("INSERT INTO `reserve`(`id`, `id_user`, `id_bike`, `duration`) 
-		VALUES (NULL,".$_POST['id_user'].",".$_POST['id_bike'].",".$_POST['duration'].",)");
+		VALUES (NULL,".$_POST['id_user'].",".$_POST['id_bike'].",".$_POST['duration'].")");
 
 	if ($query) {
 		$response["code"] = 200;
