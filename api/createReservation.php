4d63e15391214b1247640dd0e1f814acd123de5b
Amine <elouattar@gmail.com>
2018-03-03 14:28:22 +0000

    add createReservation File

---
+++ api/createReservation.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'db.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	
+	header('Content-type: Application/json');
+
+	$response = array();
+
+	$query = $db->query("INSERT INTO `reserve`(`id`, `id_user`, `id_bike`, `duration`) 
+		VALUES (NULL,".$_POST['id_user'].",".$_POST['id_bike'].",".$_POST['duration'].",)");
+
+	if ($query) {
+		$response["code"] = 200;
+	}else{
+		$response["code"] = 300;
+		$response["error"] = "Something went wrong";
+	}
+
+	echo json_encode($response);
+	//test
+
+}
+
+?>
